6a83d12b0df024dfc989b3c6c2e46a624c3b097a
ghoufranedaikra-bit <ghoufrane.daikra@univ-constantine2.dz>
2026-05-31 20:17:32 +0100

    update API URL to production

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 import axios from 'axios'
 import '@tabler/icons-webfont/dist/tabler-icons.min.css'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
+axios.defaults.baseURL = 'https://drip-backend-8i7m.onrender.com/api'
 
 const token = localStorage.getItem('token')
 if (token) {
